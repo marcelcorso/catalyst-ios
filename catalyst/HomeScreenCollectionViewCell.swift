a528b03dbfc3183241ac2981c57cc1d2f489b438
Vincent Onderwaater <vincent@onderwaater.nl>
2015-09-15 21:43:51 +0200

    stuff

--- catalyst/HomeScreenCollectionViewCell.swift
+++ catalyst/HomeScreenCollectionViewCell.swift
@@ -13,13 +13,14 @@ let noMatchNotification = "noMatchNotification"
 
 class HomeScreenCollectionViewCell: UICollectionViewCell {
     
-    var person: [String : AnyObject]?
-    
     @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dislikeButton: UIButton!
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var gradientImageView: UIImageView!
+    
+    var person: [String : AnyObject]?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -27,6 +28,34 @@ class HomeScreenCollectionViewCell: UICollectionViewCell {
         containerView.layer.shadowColor = UIColor.blackColor().CGColor
         containerView.layer.shadowOffset = CGSize(width: 0, height: 2)
         containerView.layer.shadowOpacity = 0.5
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: "moveTap:")
+        //gradientImageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func moveTap(gestureRecognizer: UIPanGestureRecognizer) {
+        
+        let trans = gestureRecognizer.translationInView(self)
+        
+        switch gestureRecognizer.state {
+        
+        case .Changed:
+            
+            print("x: \(trans)")
+            self.transform = CGAffineTransformMake(0, 0, 0, 0, trans.x, 0.0)
+           // userImageView.transform = CGAffineTransformMake(0, 0, 0, 0, trans.x, 0.0)
+            //gradientImageView.transform = CGAffineTransformMake(0, 0, 0, 0, trans.x, 0.0)
+        case .Ended:
+            self.transform = CGAffineTransformIdentity
+            userImageView.transform = CGAffineTransformIdentity
+            gradientImageView.transform = CGAffineTransformIdentity
+        default:
+            break
+            
+        }
+    
+        
+    
     }
     
     @IBAction func matchButtonTouched(sender: UIButton) {
@@ -40,10 +69,10 @@ class HomeScreenCollectionViewCell: UICollectionViewCell {
     }
     
     override func prepareForReuse() {
-    super.prepareForReuse()
+        super.prepareForReuse()
         
         person = nil
         userName.text = nil
         userImageView.image = nil
-    }    
+    }
 }
