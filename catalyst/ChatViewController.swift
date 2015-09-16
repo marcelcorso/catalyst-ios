feb6885aa23e2b5cb9b7a1dcc3fa32570d1624a9
Vincent Onderwaater <vincent@onderwaater.nl>
2015-09-16 11:02:27 +0200

    New VC for chat

---
+++ catalyst/ChatViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatViewController.swift
+//  Catalyst
+//
+//  Created by Vincent Onderwaater on 16-09-15.
+//  Copyright (c) 2015 YoloAmazeballzInterwebs. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
+extension ChatViewController : UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        
+    }
+
+    
+}
