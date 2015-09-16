0ef42e81c80aa2cbf9fe9f5bd6412682e60ea125
Vincent Onderwaater <vincent@onderwaater.nl>
2015-09-16 13:07:29 +0200

    Viewing channel is now observed

--- catalyst/HomeScreenViewController.swift
+++ catalyst/HomeScreenViewController.swift
@@ -35,7 +35,7 @@ class HomeScreenViewController: UIViewController, UIGestureRecognizerDelegate {
             
             let myId = NSUserDefaults.standardUserDefaults().objectForKey("id") as! String
             let myRef = Firebase(url:"https://catalysttv.firebaseio.com/users/\(myId)/")
-                
+            
             let likedPersonId = notification.object!["facebook_id"] as! String
             let likedPersonRef = Firebase(url:"https://catalysttv.firebaseio.com/users/\(likedPersonId)/")
             
@@ -123,16 +123,16 @@ class HomeScreenViewController: UIViewController, UIGestureRecognizerDelegate {
         // create a new message:
         let aNewMessageRef = ref.childByAutoId()
         let massage = [
-                "author_id": mineUserId,
-                "author_name": mineUserName,
-                "message": "\(arc4random_uniform(7)) John Lennon foi castigado e morto por Deus por ter dito em certa ocasião que 'Os Beatles são mais populares do que Jesus Cristo'"
+            "author_id": mineUserId,
+            "author_name": mineUserName,
+            "message": "\(arc4random_uniform(7)) John Lennon foi castigado e morto por Deus por ter dito em certa ocasião que 'Os Beatles são mais populares do que Jesus Cristo'"
         ]
         aNewMessageRef.setValue(massage)
         
     }
     
     func matchesButtonTouched(sender: UIBarButtonItem) {
-    
+        
         self.navigationController?.pushViewController(MatchesViewController(nibName: "MatchesViewController", bundle: nil), animated: true)
     }
     
@@ -166,8 +166,28 @@ class HomeScreenViewController: UIViewController, UIGestureRecognizerDelegate {
                             "facebook_id": id,
                             "name": name]
                         
-                        let ref = Firebase(url:firebaseURL + "/users/" + id)
-                        ref.setValue(userDict)
+                        let userRef = Firebase(url:firebaseURL + "/users/" + id)
+                        userRef.setValue(userDict, withCompletionBlock: { (error: NSError!, firebase: Firebase!) -> Void in
+                            let child = userRef.childByAppendingPath("viewing_channel").observeEventType(.Value, withBlock: { (channelSnapshot: FDataSnapshot!) -> Void in
+                                
+                                if let channelNumber = channelSnapshot.value as? Int {
+                                    
+                                    let potentialMatches = Firebase(url:"https://catalysttv.firebaseio.com/users")
+                                    
+                                    var queryRef = potentialMatches.queryOrderedByChild("viewing_channel").queryEndingAtValue(channelNumber)!.queryStartingAtValue(channelNumber)
+                                    
+                                    self.people.removeAll(keepCapacity: false)
+                                    
+                                    queryRef.observeEventType(.ChildAdded, withBlock: { (snapshot: FDataSnapshot!) in
+                                        
+                                        if let person = snapshot.value as? [String: AnyObject] {
+                                            self.people.append(person)
+                                        }
+                                        self.collectionView.reloadData()
+                                    })
+                                }
+                            })
+                        })
                         
                         NSUserDefaults.standardUserDefaults().setObject(id, forKey: "id")
                         NSUserDefaults.standardUserDefaults().setObject(name, forKey: "name")
@@ -176,26 +196,12 @@ class HomeScreenViewController: UIViewController, UIGestureRecognizerDelegate {
                     }
                 }
             }
-            
-            var channelNumber = 1
-            2
-            let ref = Firebase(url:"https://catalysttv.firebaseio.com/users")
-            
-            var queryRef = ref.queryOrderedByChild("viewing_channel").queryEndingAtValue(channelNumber)!.queryStartingAtValue(channelNumber)
-            
-            queryRef.observeEventType(.ChildAdded, withBlock: { (snapshot: FDataSnapshot!) in
-                
-                if let person = snapshot.value as? [String: AnyObject] {
-                    self.people.append(person)
-                }
-                self.collectionView.reloadData()
-            })
         }
     }
 }
 
 extension HomeScreenViewController : UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSize(width: CGRectGetWidth(UIScreen.mainScreen().bounds), height: 260)
     }
@@ -216,13 +222,13 @@ extension HomeScreenViewController : UICollectionViewDataSource  {
         if let urlString = person["avatar"] as? String,
             url = NSURL(string: urlString) {
                 
-            SDWebImageManager.sharedManager().downloadImageWithURL(url, options: nil, progress: nil) { (image, error, imageCacheType, finished, url) -> Void in
-                
-                if image != nil {
-                cell.userImageView.image = image
+                SDWebImageManager.sharedManager().downloadImageWithURL(url, options: nil, progress: nil) { (image, error, imageCacheType, finished, url) -> Void in
+                    
+                    if image != nil {
+                        cell.userImageView.image = image
+                    }
                 }
-            }
-        }        
+        }
         return cell
     }
     
